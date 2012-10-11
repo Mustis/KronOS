59c06b17ba85f517fbb1eb8e555943c0f30c8e7e
Conny Sjöblom <biohzn@www.mustis.org>
2012-10-11 09:22:59 +0300

    Adding CodeIgniter version

---
+++ video/application/controllers/account.php
@@ -0,0 +1,25 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Account extends CI_Controller {
+
+	public function __construct() {
+		# Required
+        parent::__construct();
+		
+		$this->load->model('user');
+	}
+
+	public function index(){
+		$test = array(
+			"This" => "Account"
+		);
+		print json_encode($test);
+	}
+	
+	public function login() {
+		if ($this->user->logged_in)
+			redirect('account/');
+			
+		echo "Login";
+	}
+}

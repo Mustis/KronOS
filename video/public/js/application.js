90b2985ca9f9fb42f1938d78bfc1a3b07d88466d
Conny Sjöblom <biohzn@mustis.org>
2012-10-11 14:57:26 +0300

    Made minor changes to the login window

--- video/public/js/application.js
+++ video/public/js/application.js
@@ -1,30 +1,42 @@
 jQuery.noConflict();
 
-jQuery(document).ready(function () {
+function loadDefaults() {
 	jQuery.getJSON("/backend/logged_in", function(data) {
 		if (!data) {
-			jQuery.ajax({
-				url: "/backend/login_modal",
-				success: function (data) { jQuery('body').append(data); },
-				dataType: 'html'
-			}).done(function() {
-				jQuery('#loginModal').modal({
-					backdrop: 'static',
-					keyboard: false
-				});
-				jQuery('#loginModal').modal('show');
-			});
+			loadLoginModal();
 		} else {
-			jQuery.getJSON("/backend/get_menu", function(data) {
-				var menuitems = []
-				jQuery.each(data, function(key, val) {
-					menuitems.push('<li><a href="' + val + '">' + key + '</li>');
-				});
-				jQuery('<ul/>', {
-					'class': 'nav',
-					html: menuitems.join('')
-				}).appendTo('.menudiv');
-			});
+			loadMenu();
 		}
 	});
-});
+}
+
+function loadLoginModal() {
+	jQuery.ajax({
+		url: "/backend/login_modal",
+		success: function (data) { jQuery('body').append(data); },
+		dataType: 'html'
+	}).done(function() {
+		jQuery('#loginModal').modal({
+			backdrop: 'static',
+			keyboard: false,
+		});
+		jQuery('#loginModal').modal('show');
+	});
+
+}
+
+function loadMenu() {
+	jQuery.getJSON("/backend/get_menu", function(data) {
+		var menuitems = []
+		jQuery.each(data, function(key, val) {
+			menuitems.push('<li><a href="' + val + '">' + key + '</li>');
+		});
+		jQuery('<ul/>', {
+			'class': 'nav',
+			html: menuitems.join('')
+		}).appendTo('.menudiv');
+	});
+
+}
+
+loadDefaults();

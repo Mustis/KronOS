536408c3151ab21366e020102ad9b3add1e3149c
Conny Sjöblom <biohzn@mustis.org>
2012-10-16 10:16:12 +0300

    Moved json to model

--- application/models/json.php
+++ application/models/json.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Json extends CI_Controller {
+class Json extends CI_Model {
 
 	public function __construct() {
 		# Required

f23a3d4cce6a522c27429877de6823746a81534f
Conny Sjöblom <biohzn@mustis.org>
2012-10-24 08:39:21 +0300

    Now the modal wont duplicate when logging out

--- public/js/application.js
+++ public/js/application.js
@@ -122,8 +122,7 @@ var wos
 					self.loadBackground();
 
 					$('#loginModal').modal('hide');
-					$('#inputUsername').val("")
-					$(".alert").remove();
+					$('#loginModal').remove();
 				} else {
 					self.showError(resp.error, 'error', '.messagebody');
 				}
@@ -140,6 +139,39 @@ var wos
  		return this;
 	};
 })( jQuery );
+
+(function( $ ) {
+        $.fn.modalHelper = function( initvar ) {
+
+	id = initvar;
+
+	this.init = function() {
+		this.append('<div id="'+this.id+'" class="modal hide fade"></div>');
+
+		this.id = '#'+this.id;
+
+		$(this.id).append('<div class="modal-header"></div>');
+		$(this.id+'>.modal-header').append('<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button><h3>unspecified</h3>');
+		$(this.id).append('<div class="modal-body"></div>');
+		$(this.id).append('<div class="modal-footer"></div>');
+	}
+
+//	this.title = function() {
+//		return
+//	}
+
+//	this.content = function() {
+//		return
+//	}
+
+	this.show = function() {
+		$(this.id).modal('show');
+	}
+
+	return this;
+	};
+})( jQuery );
+
 
 $(function () {
 	wos = $('document.body').pageConstruct();
